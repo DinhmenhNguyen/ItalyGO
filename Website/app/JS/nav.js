ebac14c25994ba417e08c1f080a6c19abf37e938
ief44 <1213842@student.roc-nijmegen.nl>
2026-06-04 13:49:04 +0200

    Onze bestemmingen toegevoegt bij contact alleen de tekst nog

--- Website/app/JS/nav.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-console.log(hamburger, dropdownMenu);
-
-hamburger.addEventListener('click', function() {
-    dropdownMenu.classList.toggle('open');
-});
