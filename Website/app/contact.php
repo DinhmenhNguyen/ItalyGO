15d57113f37c0e1c76623ba0d7134d282758ceee
ief44 <1213842@student.roc-nijmegen.nl>
2026-06-10 10:00:42 +0200

    begin gemaakt aan de contact-form, push nu zodat dinhmenh de code heeft en door kan gaan

--- Website/app/contact.php
+++ Website/app/contact.php
@@ -15,7 +15,19 @@ session_start();
     <?php
     include('includes/header.php');
     ?>
-
+<div class="contact-container">
+      <h1>Contact</h1>
+     <p>Wij helpen je graag met al je vragen over onze Italiaanse reizen.</p>
+</div>
+    <section>
+       <h1>Stuur ons een bericht</h1>
+        <form class="contact-form">
+          <input type="text" placeholder="Naam">
+          <input type="email" placeholder="E-mail">
+          <textarea placeholder="Bericht"></textarea>
+          <button type="submit">Verzenden</button>
+        </form>
+   </section>
     <footer class="footer-container">
         <div class="footer-content">
             <div class="footer-intro">
